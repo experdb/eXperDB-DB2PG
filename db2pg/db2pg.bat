b8a502ccfe9b9fc4f9a2b63cd316cb59add5d6b9
Kenneth Lee <leejw@k4m.com>
2018-08-01 13:33:30 +0900

    develop version 2.0.0

--- db2pg/db2pg.bat
+++ db2pg/db2pg.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Dfile.encoding=UTF-8 -cp ".;lib\*.jar;db2pg-1.1.3.jar" com.k4m.experdb.db2pg.Main %*
+java -Dfile.encoding=UTF-8 -cp ".;lib\*.jar;db2pg-2.0.0.jar" com.k4m.experdb.db2pg.Main %*
 @echo on
