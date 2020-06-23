a1d30aed3826843c3999beffea02aaf8518f221a
joo01224 <kimjy@k4m.com>
2020-06-23 11:25:21 +0900

    CREATE SEQUENCE 구문 뒤에 ';' 추가

--- db2pg/src/main/java/com/k4m/experdb/db2pg/convert/make/PgDDLMaker.java
+++ db2pg/src/main/java/com/k4m/experdb/db2pg/convert/make/PgDDLMaker.java
@@ -138,7 +138,7 @@ public class PgDDLMaker<T> {
 				ctsb.append("')");
 				tmpsb.append("CREATE SEQUENCE ");
 				tmpsb.append(DevUtils.classifyString(seqName,ConfigInfo.SRC_CLASSIFY_STRING));
-				tmpsb.append(String.format(" INCREMENT %d MINVALUE %d START %d", column.getSeqIncValue(), column.getSeqMinValue(), column.getSeqStart()));
+				tmpsb.append(String.format(" INCREMENT %d MINVALUE %d START %d;", column.getSeqIncValue(), column.getSeqMinValue(), column.getSeqStart()));
 				
 				tmpsb.append("\nALTER SEQUENCE ");
 				tmpsb.append(DevUtils.classifyString(seqName,ConfigInfo.SRC_CLASSIFY_STRING));
